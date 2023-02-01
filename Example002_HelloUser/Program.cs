360b49eea86a3cb4af5a750a538f9e836505eec5
V.Tischenko <smart.solutions.rnd@ya.ru>
2023-02-01 19:29:43 +0300

    добавили второй урок

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// Второй урок
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();     // ввод информации из терминала в переменную
+
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
