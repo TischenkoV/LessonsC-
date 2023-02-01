5679efe430cac332536e946faec803bfeb06026f
V.Tischenko <smart.solutions.rnd@ya.ru>
2023-02-01 19:59:01 +0300

    добавили рандомные числа в третий урок

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Третий урок, получить сумму двух целых чисел
-int numberA = 3;
-int numberB = 5;
-
+int numberA = new Random().Next(1, 10);     // случайное число от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
 
