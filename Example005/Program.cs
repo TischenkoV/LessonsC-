00be8c90157fa44ebbd0f62717cf12bc566475fe
V.Tischenko <smart.solutions.rnd@ya.ru>
2023-02-01 20:07:02 +0300

    добавили пятый урок, оригинальное приветствие

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Урок пятый, оригинальное приветствие юзеров
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower == "маша")     // .ToLower переводит все символы в нижний регистр
+{
+    Console.WriteLine("Ура! Привет Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
